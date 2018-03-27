a3cb3b019aca5927a96c4b1b583f0b7746d733a0
gopikrishnayogeshwar <36478089+gopikrishnayogeshwar@users.noreply.github.com>
2018-03-27 15:03:41 +0530

    Update module.js

--- dist/module.js
+++ dist/module.js
@@ -1,9 +1,24 @@
-import { ScatterPlotCtrl } from './scatterplot_ctrl';
-import { loadPluginCss } from 'app/plugins/sdk';
+'use strict';
 
-loadPluginCss({
-  dark: 'plugins/grafana-scatterplot-panel/css/scatterplot.dark.css',
-  light: 'plugins/grafana-scatterplot-panel/css/scatterplot.light.css',
+System.register(['./scatterplot_ctrl', 'app/plugins/sdk'], function (_export, _context) {
+  "use strict";
+
+  var ScatterPlotCtrl, loadPluginCss;
+  return {
+    setters: [function (_scatterplot_ctrl) {
+      ScatterPlotCtrl = _scatterplot_ctrl.ScatterPlotCtrl;
+    }, function (_appPluginsSdk) {
+      loadPluginCss = _appPluginsSdk.loadPluginCss;
+    }],
+    execute: function () {
+
+      loadPluginCss({
+        dark: 'plugins/grafana-scatterplot-panel/css/scatterplot.dark.css',
+        light: 'plugins/grafana-scatterplot-panel/css/scatterplot.light.css'
+      });
+
+      _export('PanelCtrl', ScatterPlotCtrl);
+    }
+  };
 });
-
-export { ScatterPlotCtrl as PanelCtrl };
+//# sourceMappingURL=module.js.map
